5d57ef06687d49a78729328aa8eb2ad52d03d4a7
l-hyeri <lhyeriii@naver.com>
2025-02-15 16:41:29 +0900

    02/15 알고리즘 study4_Comparator(문자열+길이 비교)

---
+++ src/main/java/level/level02/p_1181.java
@@ -0,0 +1,39 @@
+package level.level02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class p_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        sb.append(arr[0] + "\n");
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i] + "\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
