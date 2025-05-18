5afaff63164d0af5d8763ec5f366c5f647510718
l-hyeri <lhyeriii@naver.com>
2025-05-18 19:32:12 +0900

    05/18 알고리즘 study

---
+++ src/main/java/level/level01/p_10171.java
@@ -0,0 +1,10 @@
+package level.level01;
+
+public class p_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
