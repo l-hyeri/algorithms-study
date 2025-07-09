1f5911b4c0f037eaf6a06f25f87dceb97e39e0a0
l-hyeri <lhyeriii@naver.com>
2025-07-09 14:34:27 +0900

    07/09 알고리즘 study2_DP(2748번 Top Down 방식 코드 추가)

--- src/main/java/algorithm/DP/p_2748.java
+++ src/main/java/algorithm/DP/p_2748.java
@@ -3,8 +3,33 @@ package algorithm.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class p_2748 {
+    // [Top down 방식]
+    private static long Fibo(long[] dp, int n){
+        if (n <= 1) {
+            return dp[n] = n;   // dp[0]=0 , dp[1]=1
+        }
+
+        if (dp[n] != 0) {
+            return dp[n];   // 메모리제이션 -> 이미 값이 저장되어 있으면 계산 x
+        }
+        dp[n] = Fibo(dp, n - 1) + Fibo(dp, n - 2);
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[n + 1];
+
+        System.out.println(Fibo(dp, n));
+    }
+}
+
+/* [Bottom up 방식]
     static long[] dp = new long[91];
 
     private static long factorial(int N) {
@@ -23,4 +48,4 @@ public class p_2748 {
         int N = Integer.parseInt(br.readLine());
         System.out.println(factorial(N));
     }
-}
+* */
